ae52ff401d4b4306cf176e85ad6080181355a970
bishoybasily <bishoybasily@gmail.com>
2025-09-10 01:10:14 +0200

    cleanup

--- src/java/main/issuer/model/service/ServiceLicenses.java
+++ src/java/main/issuer/model/service/ServiceLicenses.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.function.Function;
+import static java.util.function.Function.identity;
+import static reactor.core.publisher.Mono.fromCallable;
 
 @Service
 @RequiredArgsConstructor
@@ -21,13 +22,13 @@ public class ServiceLicenses {
     private final SchedulersProvider schedulersProvider;
 
     public Flux<EntityLicense> list() {
-        return Mono.fromCallable(repositoryLicenses::findAll)
-                .flatMapIterable(Function.identity())
+        return fromCallable(repositoryLicenses::findAll)
+                .flatMapIterable(identity())
                 .subscribeOn(schedulersProvider.subscribeOn());
     }
 
     public Mono<EntityLicense> create(DtoCreateLicensePayload dto) {
-        return Mono.fromCallable(() -> {
+        return fromCallable(() -> {
 
                     final var specs = new EntityLicense.Specs()
                             .setFingerprint(dto.getFingerprint())
@@ -51,11 +52,13 @@ public class ServiceLicenses {
     }
 
     public Mono<EntityLicense> delete(String id) {
-        return Mono.fromCallable(() -> {
-                    return repositoryLicenses.findById(id).map(entity -> {
-                        repositoryLicenses.delete(entity);
-                        return entity;
-                    }).orElseThrow();
+        return fromCallable(() -> {
+                    return repositoryLicenses.findById(id)
+                            .map(entity -> {
+                                repositoryLicenses.delete(entity);
+                                return entity;
+                            })
+                            .orElseThrow();
                 })
                 .subscribeOn(schedulersProvider.subscribeOn());
     }
